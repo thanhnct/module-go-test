b031807d8c9936c5878f782f1481fcbb9dd45567
thanh.nc <congthanh29897@gmail.com>
2022-03-04 23:43:10 +0700

    update function CubeVolumn fix error

--- geometry/cube.go
+++ geometry/cube.go
@@ -1,5 +1,11 @@
 package geometry
 
-func CubeVolumn(n int) int {
-	return n * n * n
+import "errors"
+
+func CubeVolumn(n int) (int, error) {
+	if n != 0 {
+		return n * n * n, nil
+	} else {
+		return 0, errors.New("zero length edge is now allowed")
+	}
 }
